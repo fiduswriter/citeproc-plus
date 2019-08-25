69fe1398ca06809d49819183030845054ddb8f63
Johannes Wilm <mail@johanneswilm.org>
2019-08-25 22:01:38 +0200

    add wait

--- src/index.js
+++ src/index.js
@@ -4,21 +4,22 @@ import {Citeproc, styleOptions} from "citeproc-plus"
 // for citeproc-js CSL citation formatter
 
 // Get the citations that we are supposed to render, in the CSL-json format
-
-let items = [];
-let citations = [];
+// The order doesn't matter.
+let items = []
+let citations = []
+const promises = []
 for (let i=1, ilen=8;i<ilen;i++) {
-    fetch(`citations-${i}.json`).then(
+    promises.push(fetch(`citations-${i}.json`).then(
         response => response.json()
     ).then(
         json => citations = citations.concat(json)
-    );
+    ));
 
-    fetch(`items-${i}.json`).then(
+    promises.push(fetch(`items-${i}.json`).then(
         response => response.json()
     ).then(
         json => items = items.concat(json)
-    );
+    ))
 }
 
 
@@ -30,59 +31,49 @@ const citeprocSys = {
     retrieveItem(id) {
         return items.find(item => item.id === id);
     }
-};
-
-let citationStyle
-
-// Given the identifier of a CSL style, this function instantiates a CSL.Engine
-// object that can render citations in that style.
-const processor = new Citeproc();
-function getProcessor() {
-    // Instantiate and return the engine
-    return processor.getEngine(citeprocSys, citationStyle);
 }
 
 function runOneStep(idx) {
-    const citeDiv = document.getElementById('cite-div');
-    const citationParams = citations[idx];
+    const citeDiv = document.getElementById('cite-div')
+    const citationParams = citations[idx]
     const citationStrings = citeproc.processCitationCluster(citationParams[0], citationParams[1], [])[1]
     for (const citeInfo of citationStrings) {
         // Prepare node
-        const newNode = document.createElement("div");
-        newNode.setAttribute("id", `n${citeInfo[2]}`);
-        newNode.innerHTML = citeInfo[1];
+        const newNode = document.createElement("div")
+        newNode.setAttribute("id", `n${citeInfo[2]}`)
+        newNode.innerHTML = citeInfo[1]
         // Try for old node
-        const oldNode = document.getElementById(`node-${citeInfo[2]}`);
+        const oldNode = document.getElementById(`node-${citeInfo[2]}`)
         if (oldNode) {
-            citeDiv.replaceChild(newNode, oldNode);
+            citeDiv.replaceChild(newNode, oldNode)
         } else {
-            citeDiv.appendChild(newNode);
+            citeDiv.appendChild(newNode)
         }
-        newNode.scrollIntoView();
+        newNode.scrollIntoView()
     }
-    runRenderBib(idx+1);
+    runRenderBib(idx+1)
 }
-let t0;
-let t1;
+let t0
+let t1
 // This runs at document ready, and renders the bibliography
 function renderBib() {
-    t0 = performance.now();
-    runRenderBib(0);
+    t0 = performance.now()
+    runRenderBib(0)
 }
 function runRenderBib(idx) {
     if (idx === citations.length) {
-        t1 = performance.now();
-        const timeDiv = document.getElementById("time-div");
-        const timeSpan = document.getElementById("time-span");
-        timeSpan.innerHTML = `${t1 - t0} milliseconds`;
-        timeDiv.hidden = false;
+        t1 = performance.now()
+        const timeDiv = document.getElementById("time-div")
+        const timeSpan = document.getElementById("time-span")
+        timeSpan.innerHTML = `${t1 - t0} milliseconds`
+        timeDiv.hidden = false
         // Bib
-        const bibDiv = document.getElementById('bib-div');
-        const bibResult = citeproc.makeBibliography();
-        bibDiv.innerHTML = bibResult[1].join('\n');
+        const bibDiv = document.getElementById('bib-div')
+        const bibResult = citeproc.makeBibliography()
+        bibDiv.innerHTML = bibResult[1].join('\n')
     } else {
         setTimeout(() => {
-            runOneStep(idx);
+            runOneStep(idx)
         }, 0)
     }
 }
@@ -90,27 +81,23 @@ function runRenderBib(idx) {
 
 
 const styleSelector = document.querySelector('#style-selector')
+styleSelector.innerHTML += Object.entries(styleOptions).sort((a,b) => (a[1] > b[1] ? 1 : -1)).map(([key, value]) => `<option value="${key}">${value}</option>`).join('')
 
-styleSelector.innerHTML = Object.entries(styleOptions).sort((a,b) => (a[1] > b[1] ? 1 : -1)).map(([key, value]) => `<option value="${key}">${value}</option>`).join('')
-window.STYLES = Object.entries(styleOptions)
+const CSL = new Citeproc()
 
-citationStyle = styleSelector.value
+let citeproc, citationStyle
 
-let citeproc
-
-getProcessor().then(
-    proc => {
-        citeproc = proc
-        document.querySelector('#render').addEventListener('click', () => renderBib())
+Promise.all(promises).then(
+    () => {
+        styleSelector.addEventListener('change', () => {
+            citationStyle = styleSelector.value
+            CSL.getEngine(citeprocSys, citationStyle).then(
+                proc => {
+                    citeproc = proc
+                    renderBib()
+                }
+            )
+        })
+        styleSelector.removeAttribute('disabled')
     }
 )
-
-styleSelector.addEventListener('change', () => {
-    citationStyle = styleSelector.value
-    getProcessor().then(
-        proc => {
-            citeproc = proc
-            renderBib()
-        }
-    )
-})
